3fa102cb81750c7749049099ecf9831ca6dab386
ramelot <103487183+cimdataram@users.noreply.github.com>
2022-05-26 20:11:19 +0200

    Add files via upload

---
+++ caesar-chiffre.py
@@ -0,0 +1,31 @@
+# Cäser-Verschiebung // Cäsar-Algorhythmus / Cäser-Chiffre
+
+# Nachricht Verschlüsseln
+msg = 'PYTHONIC'
+key = 1
+secret_msg = ''
+
+for char in msg:
+    if not char.isalpha(): # Prüfung ob Buchstabe
+            continue
+    char = char.upper() # covert in upper case
+    code = ord(char) + key #Zahl zum Zeichen zurückgeben
+    if code > ord('Z'): # wenn größer als Z
+        code = ord('A') # Dann A draus machen
+
+    secret_msg += chr(code)
+print(secret_msg)
+
+
+# Nachricht entschlüsseln
+msg = ''
+for char in secret_msg:
+    if not char.isalpha(): # Prüfung ob Buchstabe
+            continue
+    char = char.upper() # covert in upper case
+    code = ord(char) - key #Zahl zum Zeichen zurückgeben
+    if code < ord('A'): # wenn kleiner als A
+        code = ord('Z') # Dann Z draus machen
+
+    msg += chr(code)
+print(msg)
